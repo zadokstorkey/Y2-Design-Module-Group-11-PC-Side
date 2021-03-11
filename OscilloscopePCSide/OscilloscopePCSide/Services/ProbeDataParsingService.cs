3f512828adafa5fc7eff8dbd4c5b4c28b9a087b4
Zadok Storkey <zadok@storkey.com>
2021-03-11 18:39:12 +0000

    Get the message parsing working

---
+++ OscilloscopePCSide/OscilloscopePCSide/Services/ProbeDataParsingService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OscilloscopePCSide.Services
+{
+    public class ProbeDataParsingService
+    {
+        public List<int> ParseProbeData(string message)
+        {
+            return message.Split('|').Where(numstr => numstr != "").Select(numstr => Int32.Parse(numstr)).ToList();
+        }
+    }
+}
