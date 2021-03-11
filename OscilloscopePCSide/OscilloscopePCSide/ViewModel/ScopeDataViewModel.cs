c040a652951f24685285547d7e1a06a6ce869749
Zadok Storkey <zadok@storkey.com>
2021-03-11 14:47:42 +0000

    Add the scope data viewmodel itself

---
+++ OscilloscopePCSide/OscilloscopePCSide/ViewModel/ScopeDataViewModel.cs
@@ -0,0 +1,28 @@
+﻿using GalaSoft.MvvmLight;
+using OscilloscopePCSide.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OscilloscopePCSide.ViewModel
+{
+    public class ScopeDataViewModel : ViewModelBase, IScopeDataViewModel
+    {
+        private readonly ScopeData _scopeData;
+
+        public ScopeData ScopeData
+        {
+            get
+            {
+                return _scopeData;
+            }
+        }
+
+        public ScopeDataViewModel(ScopeData scopeData)
+        {
+            this._scopeData = scopeData;
+        }
+    }
+}
