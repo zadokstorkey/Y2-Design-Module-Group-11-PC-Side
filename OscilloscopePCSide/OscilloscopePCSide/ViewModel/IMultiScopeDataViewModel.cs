6a0c47bc49aebdd90e5d62cea23273f8ff917157
Zadok Storkey <zadok@storkey.com>
2021-03-11 15:00:41 +0000

    Make a MultiScopeDataviewmodel to store all the ScopeData viewmodels

---
+++ OscilloscopePCSide/OscilloscopePCSide/ViewModel/IMultiScopeDataViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OscilloscopePCSide.ViewModel
+{
+    public interface IMultiScopeDataViewModel : INotifyPropertyChanged
+    {
+        IScopeDataViewModel Probe1ScopeDataViewModel { get; }
+        IScopeDataViewModel Probe2ScopeDataViewModel { get; }
+    }
+}
---
+++ OscilloscopePCSide/OscilloscopePCSide/ViewModel/MultiScopeDataViewModel.cs
@@ -0,0 +1,37 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OscilloscopePCSide.ViewModel
+{
+    public class MultiScopeDataViewModel : ViewModelBase
+    {
+        private readonly IScopeDataViewModel _probe1ScopeDataViewModel;
+        private readonly IScopeDataViewModel _probe2ScopeDataViewModel;
+
+        public IScopeDataViewModel Probe1ScopeDataViewModel
+        {
+            get
+            {
+                return _probe1ScopeDataViewModel;
+            }
+        }
+
+        public IScopeDataViewModel Probe2ScopeDataViewModel
+        {
+            get
+            {
+                return _probe2ScopeDataViewModel;
+            }
+        }
+
+        public MultiScopeDataViewModel(IScopeDataViewModel probe1ScopeDataViewModel, IScopeDataViewModel probe2ScopeDataViewModel)
+        {
+            this._probe1ScopeDataViewModel = probe1ScopeDataViewModel;
+            this._probe2ScopeDataViewModel = probe2ScopeDataViewModel;
+        }
+    }
+}
